63cf260a9a8e3a2213386fae674a9ff7dd9a7d93
Anish Gupta <nativeanish@gmail.com>
2023-08-18 00:42:31 +0545

    Update Arweave-wallet.ts

--- src/utils/Arweave-wallet.ts
+++ src/utils/Arweave-wallet.ts
@@ -1,9 +1,8 @@
 import { ArweaveWebWallet } from 'arweave-wallet-connector'
-
+const state = { url: 'arweave.app' }
 const wallet = new ArweaveWebWallet({
     // optionally provide information about your app that will be displayed in the wallet provider interface
     name: 'Your application name',
     logo: 'URL of your logo to be displayed to users'
-})
-wallet.setUrl('arweave.app')
+},{state:state})
 export default wallet
